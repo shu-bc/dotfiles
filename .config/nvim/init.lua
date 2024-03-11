4e91ab713e99f32d825c04604b3bd122e71c1ece
shu-bc <shu@baseconnect.in>
2024-03-11 20:43:53 +0900

    move init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,39 +1,64 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+local lazy_config = require("configs.lazy")
 
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-  },
+	{
+		"NvChad/NvChad",
+		lazy = false,
+		branch = "v2.5",
+		import = "nvchad.plugins",
+		config = function()
+			require("options")
+		end,
+	},
 
-  { import = "plugins" },
+	{ import = "plugins" },
 }, lazy_config)
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "nvchad.autocmds"
+require("nvchad.autocmds")
 
 vim.schedule(function()
-  require "mappings"
+	require("mappings")
 end)
+
+-- customized settings
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("FileType", {
+	pattern = "go",
+	callback = function()
+		vim.opt_local.expandtab = false
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.smartindent = true
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+	end,
+})
+
+autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+-- vim command autocomplete
+local opt = vim.opt
+opt.wildmode = { "list", "longest" }
+opt.wildoptions = "fuzzy"
--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,12 +1,12 @@
 local M = {}
 
-local highlights = require "highlights"
+local highlights = require("highlights")
 
 M.ui = {
-  theme = "nightfox",
+	theme = "solarized_osaka",
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
 }
 
 return M
