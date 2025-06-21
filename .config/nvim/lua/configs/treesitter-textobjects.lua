7134f7847cee70031382095898e3bccc56f0a7d5
shu <yo.shu@knowledgework.com>
2025-06-22 08:44:20 +0900

    textobjects

--- .config/nvim/lua/configs/treesitter-textobjects.lua
+++ .config/nvim/lua/configs/treesitter-textobjects.lua
@@ -1,46 +1,74 @@
-require("nvim-treesitter.configs").setup({
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			-- mappings for incremental selection (visual mappings)
-			init_selection = "gnn", -- maps in normal mode to init the node/scope selection
-			node_incremental = "grn", -- increment to the upper named parent
-			scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
-			node_decremental = "grm", -- decrement to the previous node
-		},
-	},
-	textobjects = {
-		select = {
-			enable = true,
+require("nvim-treesitter.configs").setup {
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      -- mappings for incremental selection (visual mappings)
+      init_selection = "gnn", -- maps in normal mode to init the node/scope selection
+      node_incremental = "grn", -- increment to the upper named parent
+      scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
+      node_decremental = "grm", -- decrement to the previous node
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
 
-			-- Automatically jump forward to textobj, similar to targets.vim
-			lookahead = false,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = false,
 
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-				["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-				["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-				["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+        ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+        ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+        ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
-				["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
-				["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+        ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+        ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
 
-				["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-				["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+        ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+        ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
 
-				["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-				["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+        ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+        ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
 
-				["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
-				["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+        ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+        ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
 
-				["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
-				["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+        ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+        ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
 
-				["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-				["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-			},
-		},
-	},
-})
+        ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]ia"] = { query = "@parameter.inner", desc = "Next parameter/argument start" },
+      },
+      goto_next_end = {},
+      goto_previous_start = {
+        ["[ia"] = { query = "@parameter.inner", desc = "Previous parameter/argument start" },
+      },
+      goto_previous_end = {},
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next = {
+        -- ["]d"] = "@conditional.outer",
+      },
+      goto_previous = {
+        -- ["[d"] = "@conditional.outer",
+      },
+    },
+  },
+}
+
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+-- Repeat movement with ; and ,
+-- ensure ; goes forward and , goes backward regardless of the last direction
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
