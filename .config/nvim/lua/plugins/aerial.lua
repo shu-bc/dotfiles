b979692d06bc23d5d4be5e2c54bcf623b8783274
shu <yo.shu@knowledgework.com>
2025-06-23 22:33:05 +0900

    fix aerial lua

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,22 +1,30 @@
 return {
-	{
-		"stevearc/aerial.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-			"nvim-telescope/telescope.nvim",
-		},
+  {
+    "stevearc/aerial.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-telescope/telescope.nvim",
+    },
 
-		config = function()
-			require("telescope").load_extension("aerial")
-			require("aerial").setup({
-				layout = {
-					max_width = { 60, 0.4 },
-				},
-			})
-		end,
-		event = "LspAttach",
-	},
+    config = function()
+      require("aerial").setup {
+        layout = {
+          max_width = { 60, 0.4 },
+        },
+      }
+      require("telescope").setup {
+        extensions = {
+          aerial = {
+            -- Available modes: symbols, lines, both
+            show_columns = "lines",
+          },
+        },
+      }
+      require("telescope").load_extension "aerial"
+    end,
+    event = "LspAttach",
+  },
 }
