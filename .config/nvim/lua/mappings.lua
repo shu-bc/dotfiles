04c330fed20c240002655842f99245efece2cc20
shu <yo.shu@knowledgework.com>
2025-06-23 22:41:51 +0900

    comment

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,9 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
--- map("n", ";", ":", { desc = "CMD enter command mode" })
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+-- conform.nvim
 map("n", "<leader>fm", function()
   require("conform").format()
 end, { desc = "File Format with conform" })
