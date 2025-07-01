2ece08b254d1cad96412fc311c42f8229f124e7e
shu <yo.shu@knowledgework.com>
2025-07-01 23:20:10 +0900

    nvim lazy

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,40 +2,45 @@ return {
   -- Install a plugin
   {
     "nvim-telescope/telescope.nvim",
-    opts = function(_, conf)
-      require("telescope").load_extension "fzf"
-      conf.defaults = {
-        mappings = {
-          i = { ["<c-f>"] = require("telescope.actions").to_fuzzy_refine },
+    config = function()
+      require("telescope").setup {
+        defaults = {
+          mappings = {
+            i = { ["<c-f>"] = require("telescope.actions").to_fuzzy_refine },
+          },
+          path_display = { "filename_first" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.45,
+              width = 0.85,
+            },
+          },
         },
-        path_display = { "filename_first" },
-        sorting_strategy = "ascending",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.45,
-            width = 0.85,
+        extensions = {
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
           },
         },
       }
 
-      return conf
+      require("telescope").load_extension "fzf"
     end,
-    -- config = function()
-    --   require("telescope").load_extension "fzf"
-    --   require("telescope").setup {
-    --     defaults = {
-    --       mappings = {
-    --         i = { ["<c-f>"] = require("telescope.actions").to_fuzzy_refine },
-    --       },
-    --       path_display = { "truncate" },
-    --       -- layout_config = {
-    --       --   prompt_position = "top",
-    --       -- },
-    --     },
-    --   }
-    -- end,
   },
 
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    -- install any compatible version of 0.9.x
+    version = "^0.9.0",
+    config = function()
+      require("telescope").load_extension "frecency"
+    end,
+  },
 }
---
+++ .config/nvim_lazy/lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false, -- Or true if you want it to be lazy-loaded
+  opts = {
+    picker = {
+      formatters = {
+        file = {
+          filename_first = true,
+          truncate = 60,
+        },
+      },
+      layouts = {
+        default = {
+          layout = {
+            width = 0.9,
+            height = 0.9,
+          },
+        },
+      },
+    },
+  },
+}
