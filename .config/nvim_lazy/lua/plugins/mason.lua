7b207f9d08cf867301a62d517de1ffd373351df3
shu <yo.shu@knowledgework.com>
2025-07-02 12:56:42 +0900

    add mason ensure install

---
+++ .config/nvim_lazy/lua/plugins/mason.lua
@@ -0,0 +1,26 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+
+      -- go
+      "gopls",
+      "goimports",
+      "golangci-lint-langserver",
+
+      -- python
+      "python-lsp-server",
+
+      -- terraform
+      "terraform-ls",
+
+      -- json
+      "json-lsp",
+
+      -- protobuf
+      "buf",
+    },
+  },
+}
