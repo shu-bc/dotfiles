9c1090434adcc8ac1a6e8d9ec03b54065c338f33
shu <yo.shu@knowledgework.com>
2025-07-06 18:56:04 +0900

    go-nvim

---
+++ .config/nvim_lazy/lua/plugins/go.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- lsp_keymaps = false,
+      -- other options
+    },
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+  },
+}
