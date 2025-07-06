34bd30f640b054c340aee15926b21de83e3eb94a
shu <yo.shu@knowledgework.com>
2025-07-06 23:40:38 +0900

    send selected notifiction to splitted window

--- .config/nvim_lazy/lua/plugins/snacks.lua
+++ .config/nvim_lazy/lua/plugins/snacks.lua
@@ -1,5 +1,4 @@
 -- Snacks.picker()
-
 return {
   "folke/snacks.nvim",
   lazy = false, -- Or true if you want it to be lazy-loaded
@@ -28,14 +27,40 @@ return {
           },
         },
       },
-      -- config for explorer
-      -- sources = {
-      --   explorer = {
-      --     layout = {
-      --       layout = {},
-      --     },
-      --   },
-      -- },
+      sources = {
+        -- config notifcations picker
+        notifications = {
+          actions = {
+            send = function(picker, item)
+              -- Create a temporary buffer
+              local buf = vim.api.nvim_create_buf(false, true)
+
+              -- Split window horizontally with 30% height
+              vim.cmd("botright split")
+              vim.api.nvim_win_set_height(0, math.floor(vim.o.lines * 0.3))
+
+              -- Set window wrap for long lines
+              vim.wo.wrap = true
+
+              -- Set the buffer in the new window
+              vim.api.nvim_win_set_buf(0, buf)
+
+              -- Copy item.preview.text to the buffer
+              if item.preview and item.preview.text then
+                local lines = vim.split(item.preview.text, "\n")
+                vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+              end
+            end,
+          },
+          win = {
+            input = {
+              keys = {
+                ["<CR>"] = { "send", mode = { "n", "i" } },
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
