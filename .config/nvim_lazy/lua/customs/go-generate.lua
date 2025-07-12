cd40f66fa34ad6057d0ae599f4fc2fe6e7b169df
shu <yo.shu@knowledgework.com>
2025-07-12 10:56:41 +0900

    execute async

--- .config/nvim_lazy/lua/customs/go-generate.lua
+++ .config/nvim_lazy/lua/customs/go-generate.lua
@@ -8,15 +8,34 @@ local function go_generate()
 
   vim.notify("Running go generate on " .. file_path, vim.log.levels.INFO)
 
-  local cmd = "go generate " .. file_path
-  local result = vim.fn.system(cmd)
-  local exit_code = vim.v.shell_error
+  local cmd = { "go", "generate", file_path }
+  local output = {}
 
-  if exit_code == 0 then
-    vim.notify("go generate completed successfully", vim.log.levels.INFO)
-  else
-    vim.notify("go generate failed: " .. result, vim.log.levels.ERROR)
-  end
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        vim.list_extend(output, data)
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        vim.list_extend(output, data)
+      end
+    end,
+    on_exit = function(_, exit_code)
+      if exit_code == 0 then
+        vim.notify("go generate completed successfully", vim.log.levels.INFO)
+      else
+        local error_msg = table.concat(output, "\n")
+        if error_msg == "" then
+          error_msg = "Unknown error"
+        end
+        vim.notify("go generate failed: " .. error_msg, vim.log.levels.ERROR)
+      end
+    end,
+  })
 end
 
 vim.api.nvim_create_user_command("GoGenerate", go_generate, {
