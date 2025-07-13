64b24835d7b6b7973f216d2b6eee4f93e33807b1
shu <yo.shu@knowledgework.com>
2025-07-13 20:18:35 +0900

    change bufferline keymap

---
+++ .config/nvim_lazy/lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<S-h>", false },
+    { "<S-l>", false },
+    { "<tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "<S-tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+  },
+}
